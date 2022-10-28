06a1e46880ff0f70830828bfa6618ec98c42c145
psi5059 <psi505968@gmail.com>
2022-10-28 11:34:02 +0900

    Springboot @Slf4j로 로그 남기기

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,26 +1,28 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domian.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
-
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
     @GetMapping(value = "/name")
     public String getName(){
-
+        log.info("name으로 요청이 들어왔습니다.");
         return "nayeong";
     }
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable(@PathVariable String variable){
-
+        log.info("getVariable로 요청이 들어왔습니다.");
         return variable;
     }
     @GetMapping(value = "/request1")
