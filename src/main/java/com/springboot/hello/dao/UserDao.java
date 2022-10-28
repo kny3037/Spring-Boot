3828936dd1a9ebff7a1f2b332a8b63b291022ba1
psi5059 <psi505968@gmail.com>
2022-10-28 15:31:28 +0900

    UserDao add,deleteAll, deleteById 추가

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -2,9 +2,12 @@ package com.springboot.hello.dao;
 
 import com.springboot.hello.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class UserDao {
@@ -16,12 +19,30 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
+            return user;
+        }
+    };
+
     public void add(User user){
         this.jdbcTemplate.update("insert users(id,name,password) values(?,?,?)",
                 user.getId(),user.getName(),user.getPassword());
     }
-
-    public int deleteAll(){
-        return this.jdbcTemplate.update("delete from users");
+    public User select(String id) {
+        String sql = "Select * from user where id = ?";
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
+
+    public void deleteAll(){
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public void deleteById(String id){
+        this.jdbcTemplate.update("delete * from users where id=?;",id);
+    }
+
 }
