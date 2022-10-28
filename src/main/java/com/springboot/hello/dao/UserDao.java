d4be70db8a1ab07c9a2c3c97de2edf5514e1057a
psi5059 <psi505968@gmail.com>
2022-10-28 15:02:31 +0900

    UserDao add,deleteAll 추가

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -1,14 +1,24 @@
 package com.springboot.hello.dao;
 
+import com.springboot.hello.User;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+import javax.sql.DataSource;
+
+@Repository
 public class UserDao {
     private final JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
 
-    public UserDao(JdbcTemplate jdbcTemplate) {
+    public UserDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user){
+        this.jdbcTemplate.update("insert users(id,name,password) values(?,?,?)",
+                user.getId(),user.getName(),user.getPassword());
     }
 
     public int deleteAll(){
