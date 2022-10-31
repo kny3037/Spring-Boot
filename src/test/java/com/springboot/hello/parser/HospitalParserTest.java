8e96cec4eaedd1e664470887d16012342cd4b59d
psi5059 <psi505968@gmail.com>
2022-10-31 15:30:51 +0900

    Failed to load ApplicationContext 에러 해결중..!

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -3,14 +3,35 @@ package com.springboot.hello.parser;
 import com.springboot.hello.domian.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class HospitalParserTest {
 
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Test
+    @DisplayName("10만건 이상의 데이터가 파싱 되는지.")
+    void oneHundreadThousandRows() throws IOException {
+        String filename = "C:\\Users\\psi50\\Desktop\\fulldata_01_01_02_P_의원.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 10000);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(hospitalList.get(i).getHospitalName());
+        }
+        System.out.printf("파싱된 데이터 개수:", hospitalList.size());
+    }
 
     @Test
     @DisplayName("csv 1줄을 Hospital로 잘 만드는 지 Test")
@@ -35,6 +56,8 @@ class HospitalParserTest {
         assertEquals(0, hospital.getPatientRoomCount());  //col : 30
         assertEquals(0, hospital.getTotalNumberOfBeds());  //col : 31
         assertEquals(52.29f, hospital.getTotalAreaSize());  //col : 32
+
+
 
 
     }
