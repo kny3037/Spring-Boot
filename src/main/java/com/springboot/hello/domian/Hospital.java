c085413026c159a1ea7d5b1fc28cac2fd0a452bb
psi5059 <psi505968@gmail.com>
2022-10-31 14:22:26 +0900

    HospitalParserTest

---
+++ src/main/java/com/springboot/hello/domian/Hospital.java
@@ -0,0 +1,50 @@
+package com.springboot.hello.domian;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class Hospital {
+    private int id;
+    private String openServiceName;  //Camel case
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
