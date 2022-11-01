c0f9adf8e0afc7c64e304bb6bc5cd6734990c0ad
psi5059 <psi505968@gmail.com>
2022-11-01 13:32:54 +0900

    sql insert Query값 매핑

--- src/main/java/com/springboot/hello/parser/HospitalParser.java
+++ src/main/java/com/springboot/hello/parser/HospitalParser.java
@@ -10,6 +10,24 @@ public class HospitalParser implements Parser<Hospital>{
     @Override
     public Hospital parse(String str) {
 
+//1, Id()); // col:0
+//"의원", OpenServiceName());//col:1
+//3620000,OpenLocalGovernmentCode()); // col: 3
+//"PHMA119993620020041100004",ManagementNumber()); // col:4
+//LocalDateTime.of(1999, 6, 12, 0, 0, 0), LicenseDate()); //19990612 //col:5
+//1, BusinessStatus()); //col:7
+//13, BusinessStatusCode());//col:9
+//"062-515-2875", Phone());//col:15
+//"광주광역시 북구 풍향동 565번지 4호 3층", FullAddress()); col:18
+//"광주광역시 북구 동문대로 24, 3층 (풍향동)", RoadNameAddress());//col:19
+//"효치과의원", HospitalName());//col:21
+//"치과의원", BusinessTypeName());//col:25
+//1, HealthcareProviderCount()); //col:29
+//0, PatientRoomCount()); //col:30
+//0, TotalNumberOfBeds()); //col:31
+//52.29, TotalAreaSize()); //col:32
+
+
         String[] row = str.split("\",\"");
         System.out.println(Arrays.toString(row));
 
