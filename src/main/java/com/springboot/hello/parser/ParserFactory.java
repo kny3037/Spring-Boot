4afc344476b367e8738ac70e151a17c2afb3193e
psi5059 <psi505968@gmail.com>
2022-11-01 13:33:29 +0900

    annotation 수정

--- src/main/java/com/springboot/hello/parser/ParserFactory.java
+++ src/main/java/com/springboot/hello/parser/ParserFactory.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ParserFactory {
-
     @Bean
     public ReadLineContext<Hospital> hospitalReadLineContext(){
         return new ReadLineContext<Hospital>(new HospitalParser());
